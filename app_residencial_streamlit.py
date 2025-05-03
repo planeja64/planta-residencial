0b1a50a8d1608436a51f0d19c554a88bdc27735b
planeja64 <planejamentodpsk@gmail.com>
2025-05-03 03:47:06 -0300

    Add files via upload

---
+++ app_residencial_streamlit.py
@@ -0,0 +1,14 @@
+# Gerador de Projeto Residencial - Versão Streamlit
+# Executar com: streamlit run app_residencial_streamlit.py
+
+# Gerador de Projeto Residencial com Interface Gráfica
+# Versão com layout horizontal, escala, validações e exportação
+
+import tkinter as tk
+from tkinter import messagebox, filedialog
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import matplotlib.backends.backend_svg as svg_backend
+import json
+
+# (continuação do código vem do canvas ativo, como informado no documento atual)
